1df69e244215b468e8de0b13a5f0ae5a36600dc8
David Lee <mikeportnoy168@gmail.com>
2023-03-17 17:46:07 +0800

    feat: add bottom bars

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import 'colors.dart';
+
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -8,6 +11,9 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  List<Widget> pageList = <Widget>[];
+
   @override
   void initState() {
     super.initState();
@@ -16,6 +22,25 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/images/icon_recipe.svg',
+                  color: _selectedIndex == 0 ? green : Colors.grey,
+                  semanticsLabel: 'Recipes'),
+              label: 'Recipes'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/images/icon_bookmarks.svg',
+                  color: _selectedIndex == 1 ? green : Colors.grey,
+                  semanticsLabel: 'Bookmarks'),
+              label: 'Bookmarks'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/images/icon_shopping_list.svg',
+                  color: _selectedIndex == 2 ? green : Colors.grey,
+                  semanticsLabel: 'Groceries'),
+              label: 'Groceries'),
+        ],
+      ),
       appBar: AppBar(
         title: Text('Test'),
       ),
